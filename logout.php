d046f79ed161013c9b797655bd19507aaeb32ad8
dasha-alekseenkova <72573782+dasha-alekseenkova@users.noreply.github.com>
2020-12-28 08:47:10 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION["first_name"] = null;
-$_SESSION["last_name"] = null;
-$_SESSION["email"] = null;
-$_SESSION["id"] = null;
-$_SESSION["role"] = null;
-header("Location:/test1/homepage.php");
